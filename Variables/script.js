c0063712208a8fb30e8e750f20099a6a586bd69a
silverdale-muhammadmohsinmushtaq <mohsin.mushtaq@silverdaletech.com>
2024-12-22 10:19:17 -0800

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,9 @@
+var FirstVariable = "Hello World!zzzz";
+const y = 10;
+let x = "Hello World 2"
+console.log(FirstVariable);
+console.log(x);
+console.log(y);
+
+x = "Hello World 3";
+console.log(x);
